a31a30781522ee36d207a80ab5fb3fbc842717dc
kartik mishra <mishrakartik672@gmail.com>
2024-08-18 18:39:08 +0530

    Add files via upload

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 66;
+let accountEmail = "kartik@gmail.com"
+var accountPassword = "7454"
+accountCity = "meerut"
+let accountState = "Uttar Pradesh"
+
+// accountId = 23
+// console.log(accountId)
+
+accountEmail = "gym@notLover"
+accountPassword = "1234"
+accountCity = "Noida"
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
