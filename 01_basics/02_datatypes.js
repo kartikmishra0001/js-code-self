f62f4a98ac2f1a9450fee1dd6dd6becb840e9f38
kartik mishra <mishrakartik672@gmail.com>
2024-08-18 18:39:48 +0530

    Add files via upload

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"
+// treat all js code as newer version
+
+// alert("hello")
+
+/* 
+for Documentation Go to -: 
+   https://tc39.es/ecma262/
+   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide
+*/
+
+// Data Types
+// 1. Primitive Data Types
+// Number
+// String
+// Boolean
+// Null -> standalone value
+// Undefined
+// Symbol (new in ES6) -> unique
+
+// 2. Non-Primitive Data Types
+// Object
+// Array
+// Function
+
+console.table([typeof "kartik", typeof 1234, typeof false, typeof null, typeof undefined]);
