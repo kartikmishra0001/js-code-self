6a918ef8804175969f79ccb35334d9a5269ba1f3
kartik mishra <mishrakartik672@gmail.com>
2024-08-20 21:39:20 +0530

    Add files via upload

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,77 @@
+let score = 66;
+// let score = "kartik mishra";
+// let score = false;
+// let score = null;
+// let score = undefined;
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let variable = "kartik";
+// let variable = 23;
+// let variable = undefined;
+// let variable = null;
+
+let checkVariable = String(variable);
+// console.log(typeof checkVariable);
+// console.log(checkVariable);
+
+let boolean = false;
+// let boolean = "kartik";
+// let boolean = 0;
+
+let checkBoolean = Boolean(boolean);
+// console.log(typeof checkBoolean);
+// console.log(checkBoolean);
+
+// ************************************** Operation *********************************************
+
+let value = 66;
+let NegValue = -value;
+// console.log(NegValue);
+
+// Operations
+/*
+1. Addition -> (+)
+2. Subtraction -> (-)
+3. Divide -> (/)
+4. Module -> (%)
+5. Power -> (**)
+6. Multiplicatino -> (*)
+*/
+
+// Add Two String 
+let str1 = "kartik ";
+let str2 = "mishra";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// conversion
+// console.log("1" + 2); -> 12
+// console.log(1 + "2"); -> 12
+// console.log("1" + 2 + 2); -> 122
+// console.log(1 + 3 + "2"); -> 42
+
+// If you want to know what is happend then check link
+// https://262.ecma-international.org/#sec-tonumber-applied-to-the-string-type
+
+// console.log(3 + 4 * 5 / 6)
+// Ignore this type of contex because this is not good way to write
+
+// console.log(true);
+// console.log(+true); // you write like that 
+// console.log(+"")
+// console.log(true+); // but not like that this is incorrect
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 5 + 5 // don't write like that because they decrease readibility
+
+let gameCounter = 100;
+// gameCounter++;
+// ++gameCounter;
+console.log(gameCounter);
+
+// learn about prefix or postfix
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
