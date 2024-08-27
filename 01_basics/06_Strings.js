50fdba48ea434d3d7ada97818d78968373ef7a25
kartik mishra <mishrakartik672@gmail.com>
2024-08-28 00:07:11 +0530

    Add files via upload

---
+++ 01_basics/06_Strings.js
@@ -0,0 +1,93 @@
+// const name = "kartik";
+// const name2 = "vikas"
+
+// don't use this type of Syntax to add String 
+// console.log(name + name2);
+
+// use this 
+// console.log(`My name is ${name} and My Friend name is ${name2}`);
+
+// Second way to Implement String
+// const userName = new String('phenius-ferb');
+// console.log(userName);
+
+// String all function list 
+// if you want master on String then check this and all are Function
+
+/* Start here -----------
+
+length
+at() 
+big() 
+blink()
+bold()
+charAt()
+charCodeAt()
+concat()
+String() 
+endsWith() 
+fixed() 
+fontcolor()
+fontsize() 
+includes()
+indexOf()
+isWellFormed()
+italics()
+lastIndexOf()
+link()
+localeCompare()
+match()
+matchAll()
+normalize()
+padEnd()
+padStart()
+repeat()
+replace()
+replaceAll()
+search()
+slice()
+small()
+split()
+startsWith()
+strike()
+sub()
+substr()
+substring()
+sup()
+toLocaleLowerCase()
+toLocaleUpperCase()
+toLowerCase()
+toString()
+toUpperCase()
+toWellFormed()
+trim()
+trimEnd()
+trimStart()
+valueOf()
+[Symbol.iterator]()
+
+------------ end here*/
+
+const userName = new String('ben-10');
+// console.log(userName[2]);
+
+// Some function 
+// console.log(userName.length);
+// console.log(userName.toUpperCase());
+
+const cartoonName = '         Doreamon-Nobita'
+// console.log(cartoonName.trim());
+// console.log(userName.charAt(4));
+// console.log(userName.indexOf('n'));
+
+const otherOne = new String('Ninja-Hatori')
+// console.log(otherOne.substring(0,5));
+// console.log(otherOne.slice(0,4));
+
+const CreateEmail = "kartikbhai!@w42google.com";
+// console.log(CreateEmail.replace('!@w42','king@'));
+
+// check character is present in variable
+// console.log(CreateEmail.includes('bhai'))
+
+console.log(userName.split('-'));
