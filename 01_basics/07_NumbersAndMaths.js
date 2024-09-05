da5b32afa1d477372dbe5cb4f79db29881d3ca93
kartik mishra <mishrakartik672@gmail.com>
2024-09-05 11:42:40 +0530

    Add files via upload

---
+++ 01_basics/07_NumbersAndMaths.js
@@ -0,0 +1,31 @@
+const Score = 400;
+// console.log(Score);
+
+const Balance = new Number(100);
+// console.log(Balance);
+// console.log(Balance.toString().length);
+// console.log(Balance.toFixed(2));
+
+const otherNumber = 23.494
+// console.log(otherNumber.toPrecision(3));
+
+const CountZeros = 100000000000
+// console.log(CountZeros.toLocaleString('en-IN'));
+
+// ********************************************************* Math ********************************************************
+
+// console.log(Math);
+// console.log(Math.abs(-67));
+// console.log(Math.round(23.634));
+// console.log(Math.floor(3.6364));
+// console.log(Math.ceil(3.45));
+// console.log(Math.max(3,5,345,2345,3245,34));
+// console.log(Math.min(1234,243,234,234,234,4,23));
+
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1 );
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * ((max - min ) + 1) + min));
